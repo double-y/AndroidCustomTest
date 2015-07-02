7a870a5e08181f37de34ae5ae98f0f36bafe26b7
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-07-02 22:40:35 +0900

    init

---
+++ customtest/src/main/java/com/doubley/customandroidtest/customtest/CustomInstrumentationTestCase.java
@@ -0,0 +1,9 @@
+package com.doubley.customandroidtest.customtest;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by yasudayousuke on 7/2/15.
+ */
+public class CustomInstrumentationTestCase extends InstrumentationTestCase {
+}
